07b43ddb6274bb2ebc0d7b4b67ddbf09c8e4598e
Marc Labud <marc.labud@hsr.ch>
2016-09-21 14:59:12 +0200

    setup appp with signup and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { routing } from './app.routing';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,17 @@
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import {SignupComponent}      from './signup/signup.component';
+import {LoginComponent}      from './login/login.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
