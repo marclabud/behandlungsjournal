f459850886730bc0b97d6954158e0498b11a3a05
Marc Labud <marc.labud@hsr.ch>
2016-09-23 09:24:03 +0200

    SignupFormImplemented

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormControl, FormBuilder, Validators} from "@angular/forms";
+import {IUser} from "../shared/user";
 
 @Component({
   selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  templateUrl: './signup.component.html'
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  public SignupForm: FormGroup;
+  public submitted: boolean;
+  constructor(private _formbuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.SignupForm = this._formbuilder.group({
+      name: ['',[<any>Validators.required]],
+      email: ['',[<any>Validators.required]],
+      password: ['',[<any>Validators.required]]
+    })
   }
-
+  save(model: IUser, isValid: Boolean) {
+    this.submitted = true; // set form submit to true
+    console.log(model,isValid)
+  }
 }
