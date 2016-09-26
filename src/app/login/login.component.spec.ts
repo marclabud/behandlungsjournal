a3656eff44dab77ba4e2ac920b660e185d0fa195
Marc Labud <marc.labud@hsr.ch>
2016-09-26 17:24:27 +0200

    tests for login

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -2,12 +2,50 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
-import { FormBuilder } from '@angular/forms'
+import { FormBuilder, Validators } from '@angular/forms'
+import { IUser} from "../shared/authorisation/user";
 
 describe('Component: Login', () => {
   it('should create an instance', () => {
     let formbuilder= new FormBuilder() ;
     let component = new LoginComponent(formbuilder);
     expect(component).toBeTruthy();
+  });
+  describe('ngOnInit', () => {
+    it('should create form group with email and password', () => {
+      let formbuilder = new FormBuilder();
+      spyOn(formbuilder, 'group');
+      let component = new LoginComponent(formbuilder);
+
+      component.ngOnInit();
+
+      expect(formbuilder.group).toHaveBeenCalledWith({
+        email: ['', [<any>Validators.required]],
+        password: ['', [<any>Validators.required]]
+      });
+    });
+  });
+
+  describe('onSubmit', () => {
+    it('should call addUser on UserService if isValid is true', () => {
+      let formbuilder = new FormBuilder();
+      let component = new LoginComponent(formbuilder);
+      let user = {name: 'rudi', email: 'cant@fail.io', password: 'oops'};
+      let isValid = true;
+
+      component.onSubmit(user, isValid);
+
+      //expect()
+    });
+    it('should do nothing if isValid is false', () => {
+      let formbuilder = new FormBuilder();
+      let component = new LoginComponent(formbuilder);
+      let user = {name: 'rudi', email: 'cant@fail.io', password: 'oops'};
+      let isValid = false;
+
+      component.onSubmit(user, isValid);
+
+      //expect()
+    });
   });
 });
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from "@angular/core";
-import {FormGroup,FormControl, FormBuilder, Validators} from "@angular/forms";
+import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 import { IUser} from "../shared/authorisation/user";
 
 @Component({
