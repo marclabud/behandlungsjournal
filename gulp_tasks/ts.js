8b129014f3e32bcd3df1f96e983990bdd056e320
Marc Labud <marc.labud@hsr.ch>
2016-10-13 21:21:51 +0200

    gulp-typescript update 3.0.3 sucessful after moving server dir

--- gulp_tasks/ts.js
+++ gulp_tasks/ts.js
@@ -5,27 +5,27 @@ const gts = require('gulp-typescript');
 const tsProject = gts.createProject("server/tsconfig.json");
 // Gulp typescript 3.0.2
 
-// gulp.task('ts', tsc);
-//
-// function tsc() {
-//     gutil.log("tsc aufruf");
-//     tsProject.src()
-//         .on('error', gutil.log)
-//         .pipe(tsProject())
-//         .pipe(gulp.dest("../dist_server"));
-// }
-// tsc.description=`ExpressServer: JS-Generierung mit Typescript (tsconfig.json)`;
-
-
-// Gulp typescript 2.14.1
 gulp.task('ts', tsc);
 
 function tsc() {
-  gutil.log("tsc aufruf");
-  tsProject.src()
-    .on('error', gutil.log)
-    .pipe(gts(tsProject))
-    .pipe(gulp.dest("build/"));
+    gutil.log("tsc aufruf");
+    tsProject.src()
+        .on('error', gutil.log)
+        .pipe(tsProject())
+        .pipe(gulp.dest("./"));
 }
-tsc.description=`JS-Generierung mit Typescript (tsconfig.json)`;
-//typescript 1.8.10 ? Bitte keine zwei compilerversionen in einem Projekt.
+tsc.description=`ExpressServer: JS-Generierung mit Typescript (tsconfig.json)`;
+
+
+// Gulp typescript 2.14.1
+// gulp.task('ts', tsc);
+//
+// function tsc() {
+//   gutil.log("tsc aufruf");
+//   tsProject.src()
+//     .on('error', gutil.log)
+//     .pipe(gts(tsProject))
+//     .pipe(gulp.dest("./"));
+// }
+// tsc.description=`JS-Generierung mit Typescript (tsconfig.json)`;
+// //typescript 1.8.10 ? Bitte keine zwei compilerversionen in einem Projekt.
