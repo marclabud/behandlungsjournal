eb5f2d56eb7383e5c4992283cb7b6a802912d9ee
Marc Labud <marc.labud@hsr.ch>
2016-10-16 16:50:19 +0200

    linting now in package.json tslint for client with $npm run lint tslint for server with $npm run lintserver

--- gulp_tasks/lint.js
+++ gulp_tasks/lint.js
@@ -2,13 +2,19 @@
 const gulp = require('gulp');
 const tslint = require('gulp-tslint');
 const conf = require('../conf/gulp.conf');
-
+// ToDO: Setup gulp-tslint for right  configuration
+// I guess he doesnt find the tsconfig.json
+//tslint commandline works with:
+// "tslint --project src/tsconfig.json" for client
+// "tslint --project server/tsc/tsconfig.json" for server
+//
 console.log ();
 gulp.task('lint:server', lint);
 
 function lint() {
-return gulp.src('server/src/**/*.ts')
+return gulp.src('*.ts')
   .pipe(tslint({
+    project: 'src/tsconfig.json',
     formatter: 'verbose'
   }))
 }
