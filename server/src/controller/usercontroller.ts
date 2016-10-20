91c2e0add19619a7264e009ffa2b532dae9a0699
Marc Labud <marc.labud@hsr.ch>
2016-10-20 18:18:02 +0200

    login error fixed typo in code

--- server/src/controller/usercontroller.ts
+++ server/src/controller/usercontroller.ts
@@ -3,81 +3,83 @@ import * as _ from 'lodash';
 
 const User = require ('../models/user.model');
 
-module.exports.getAllUsers = (req, res) => {
+module.exports.getAllUsers = (request, response) => {
   User.find({}, (err, docs) => {
     if (err) {
       return console.error(err);
     }
-    res.status(200).json(docs);
+    response.status(200).json(docs);
   });
 };
 
-module.exports.countUsers = (req, res) => {
+module.exports.countUsers = (request, response) => {
   User.count((err, count) => {
     if (err) {
       return console.error(err);
     }
-    res.status(200).json(count);
+    response.status(200).json(count);
   });
 };
 
-module.exports.addUser = (req, res) => {
-  let UsertoUpdate = new User(req.body);
+module.exports.addUser = (request, response) => {
+  let UsertoUpdate = new User(request.body);
   UsertoUpdate.save((err) => {
     if (err) {
       return console.error(err);
     }
-    res.status(200).json(UsertoUpdate);
+    response.status(200).json(UsertoUpdate);
   });
 };
 
-module.exports.findUserbyId = (req, res) => {
-  User.findOne({_id: req.params.id}, (err, docs) => {
+module.exports.findUserbyId = (request, response) => {
+  User.findOne({_id: request.params.id}, (err, docs) => {
     if (err) {
       return console.error(err);
     }
-    res.sendStatus(200).json(docs);
+    response.sendStatus(200).json(docs);
   });
 };
 // update User by Id
-module.exports.updateUser = (req, res) => {
-  User.findOneAndUpdate({_id: req.params.id}, req.body, (err) => {
+module.exports.updateUser = (request, response) => {
+  User.findOneAndUpdate({_id: request.params.id}, request.body, (err) => {
     if (err) {
       return console.error(err);
     }
-    res.sendStatus(200);
+    response.sendStatus(200);
   });
 };
 
-module.exports.deleteUser = (req, res) => {
-  User.findOneAndRemove({_id: req.params.id}, (err) => {
+module.exports.deleteUser = (request, response) => {
+  User.findOneAndRemove({_id: request.params.id}, (err) => {
     if (err) {
       return console.error(err);
     }
-    res.sendStatus(200);
+    response.sendStatus(200);
   });
 };
 
-// module.exports.loginUser = (res, req) => {
-//   let email: string = req.body.email;
-//   let password: string = req.body.password;
-//
-//   if (email === '' || password === '') {
-//     return res.status(400).send('You must send the username and the password');
-//   }
-//   // mongodbquery
-//   User.find({email: email, password: password}, (err: any, docs: any): any => {
-//     if (err) {
-//       return console.error(err);
-//     }
-//     if (!(_.isEmpty(docs))) {
-//       // 1 User wurde gefunden; Token wird zurückgesendet
-//       // ToDo: Check mit count, ob es mehr als einen User gibt.
-//       let token = '123';
-//       res.status(201).send({id_token: token});
-//       console.log('Token', 'Token to response ok');
-//     } else {    // keinen User mit diesem Passwort gefunden
-//       return res.status(401).send({message: 'The username or password dont match', email: email});
-//     }
-//   });
-// };
+module.exports.loginUser = (request, response) => {
+  let email = '';
+  email = request.body.email;
+  let password = '';
+  password = request.body.password;
+
+  if (email === '' || password === '') {
+    return response.status(400).send('You must send the username and the password');
+  }
+  // mongodbquery
+  User.find({email: email, password: password}, (err: any, docs: any): any => {
+    if (err) {
+      return console.error(err);
+    }
+    if (!(_.isEmpty(docs))) {
+      // 1 User wurde gefunden; Token wird zurückgesendet
+      // ToDo: Check mit count, ob es mehr als einen User gibt.
+      let token = '123';
+      response.status(201).send({id_token: token});
+      console.log('Token', 'Token to response ok');
+    } else {    // keinen User mit diesem Passwort gefunden
+      return response.status(401).send({message: 'The username or password dont match', email: email});
+    }
+  });
+};
