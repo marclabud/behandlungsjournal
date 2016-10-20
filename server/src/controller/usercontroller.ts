00c29ab55c9ac2e7dcbd29c512424e4767fb4f60
Marc Labud <marc.labud@hsr.ch>
2016-10-20 15:14:11 +0200

    routes and usercontroller refactoring  dbqueries moved to controller, removed all tslinterrors in server

--- server/src/controller/usercontroller.ts
+++ server/src/controller/usercontroller.ts
@@ -1,13 +1,83 @@
 'use strict';
+import * as _ from 'lodash';
 
-// Models
-const UserModel = require('../models/user.model');
+const User = require ('../models/user.model');
 
-module.exports.getAllUsers = function(req, res) {
-  UserModel.find({}, function (err, docs) {
+module.exports.getAllUsers = (req, res) => {
+  User.find({}, (err, docs) => {
     if (err) {
       return console.error(err);
     }
     res.status(200).json(docs);
+  });
+};
+
+module.exports.countUsers = (req, res) => {
+  User.count((err, count) => {
+    if (err) {
+      return console.error(err);
+    }
+    res.status(200).json(count);
+  });
+};
+
+module.exports.addUser = (req, res) => {
+  let UsertoUpdate = new User(req.body);
+  UsertoUpdate.save((err) => {
+    if (err) {
+      return console.error(err);
+    }
+    res.status(200).json(UsertoUpdate);
+  });
+};
+
+module.exports.findUserbyId = (req, res) => {
+  User.findOne({_id: req.params.id}, (err, docs) => {
+    if (err) {
+      return console.error(err);
+    }
+    res.sendStatus(200).json(docs);
+  });
+};
+// update User by Id
+module.exports.updateUser = (req, res) => {
+  User.findOneAndUpdate({_id: req.params.id}, req.body, (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    res.sendStatus(200);
+  });
+};
+
+module.exports.deleteUser = (req, res) => {
+  User.findOneAndRemove({_id: req.params.id}, (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    res.sendStatus(200);
+  });
+};
+
+module.exports.loginUser = (res, req) => {
+  let email: string = req.body.email;
+  let password: string = req.body.password;
+
+  if (email === '' || password === '') {
+    return res.status(400).send('You must send the username and the password');
+  }
+  // mongodbquery
+  User.find({email: email, password: password}, (err: any, docs: any): any => {
+    if (err) {
+      return console.error(err);
+    }
+    if (!(_.isEmpty(docs))) {
+      // 1 User wurde gefunden; Token wird zurückgesendet
+      // ToDo: Check mit count, ob es mehr als einen User gibt.
+      let token = '123';
+      res.status(201).send({id_token: token});
+      console.log('Token', 'Token to response ok');
+    } else {    // keinen User mit diesem Passwort gefunden
+      return res.status(401).send({message: 'The username or password dont match', email: email});
+    }
   });
 };
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,79 +1,29 @@
 'use strict';
 import * as express from 'express';
-import * as _ from 'lodash';
 const UserController = require ('../controller/usercontroller');
-const userRouter = express.Router();
 
+const userRouter = express.Router();
 
 // APIs
 // select all users
 userRouter.get('/users', UserController.getAllUsers);
 
 // count all users
-userRouter.get('/users/count', function (req, res) {
-  User.count(function (err, count) {
-    if (err) return console.error(err);
-    res.status(200).json(count);
-  });
-});
+userRouter.get('/users/count', UserController.countUsers);
 
 // create new user
-userRouter.post('/user', function (req, res) {
-  let obj = new User(req.body);
-  obj.save(function (err, obj) {
-    if (err) return console.error(err);
-    res.status(200).json(obj);
-  });
-});
+userRouter.post('/user', UserController.addUser);
 
 // find user by id
-userRouter.get('/user/:id', function (req, res) {
-  User.findOne({_id: req.params.id}, function (err, docs) {
-    if (err) {
-      return console.error(err);
-    }
-    res.status(200).json(docs);
-  });
-});
+userRouter.get('/user/:id', UserController.findUserbyId);
 
 // update user by id
-userRouter.put('/user/:id', function (req, res) {
-  User.findOneAndUpdate({_id: req.params.id}, req.body, function (err) {
-    if (err) return console.error(err);
-    res.sendStatus(200);
-  })
-});
+userRouter.put('/user/:id', UserController.updateUser);
 
 // delete user by id
-userRouter.delete('/user/:id', function (req, res) {
-  User.findOneAndRemove({_id: req.params.id}, function (err) {
-    if (err) return console.error(err);
-    res.sendStatus(200);
-  });
-});
+userRouter.delete('/user/:id', UserController.deleteUser);
 
 // user login
-userRouter.post('/user/login', function (req, res) {
-  // ToDo: Fehlerbehandlung, falls im Body ein Array zurückkommt und kein String.
-  let email: string = req.body.email;
-  let password: string = req.body.password;
+userRouter.post('/user/login', UserController.loginUser);
 
-  if (email === '' || password === '') {
-    return res.status(400).send('You must send the username and the password');
-  }
-  // mongodbquery
-  User.find({email: email, password: password}, function (err: any, docs: any): any {
-    if (err) return console.error(err);
-    if (!(_.isEmpty(docs))) {
-      // 1 User wurde gefunden; Token wird zurückgesendet
-      // ToDo: Check mit count, ob es mehr als einen User gibt.
-      let token = '123';
-      res.status(201).send({id_token: token});
-      console.log('Token', 'Token to response ok');
-    }
-    else {    // keinen User mit diesem Passwort gefunden
-      return res.status(401).send({message: 'The username or password dont match', email: email});
-    }
-  });
-});
 export= userRouter;
