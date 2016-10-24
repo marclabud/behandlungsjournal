b7db10ccdb565dc5130c74ec7a1a0debc1e35ada
Marc Labud <marc.labud@hsr.ch>
2016-10-24 06:29:53 +0200

    gulp task watch watches now client and server sources

--- gulp_tasks/watch.js
+++ gulp_tasks/watch.js
@@ -6,10 +6,27 @@ const conf = require('../conf/gulp.conf');
 
 const globPatternWatch ='/**/*.ts';
 
-gulp.task('watch', watch);
+gulp.task ('watch', ['watch_client','watch_server'],watch);
 
 function watch() {
+  gutil.log('watch all started');
+}
+watch.description='watch all project typescript sources';
+
+gulp.task('watch_client', watch_client);
+gulp.task('watch_server', watch_server);
+
+
+function watch_client() {
+  gutil.log(conf.paths.src+ globPatternWatch);
+  gulp.watch(conf.paths.src+ globPatternWatch,['build_client'] );
+}
+watch_client.description='watch client sources';
+
+
+function watch_server() {
   gutil.log(conf.paths.srcServer+ globPatternWatch);
   gulp.watch(conf.paths.srcServer+ globPatternWatch,['build_server'] );
 }
-watch.description='watch';
+watch_server.description='watch_server sources';
+
