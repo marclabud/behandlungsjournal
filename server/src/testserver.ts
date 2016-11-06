c0550d797d1242f6a052160278ac456669d47721
Marc Labud <marc.labud@hsr.ch>
2016-11-06 20:03:14 +0100

    fixed lint errors

--- server/src/testserver.ts
+++ server/src/testserver.ts
@@ -23,17 +23,41 @@ server.dataStore.save(
 
 // Mockdaten für api/patients laden
 server.dataStore.save(
-  new Resource('/api/patients/Strolch', {name: 'Strolch', tierart: 'Hund', rasse: 'Mischling', diagnose: 'Bisswunde', eigentuemerVorname: 'Hans', eigentuemerNachname: 'Iten'}),
-  new Resource('/api/patients/Minka', {name: 'Minka', tierart: 'Katze', rasse: 'Siam', diagnose: 'Vergiftung', eigentuemerVorname: 'Maria', eigentuemerNachname: 'Rogenmoser'}),
-  new Resource('/api/patients/Blacky', {name: 'Blacky', tierart: 'Pferd', rasse: 'Freiberger', diagnose: 'Stauchung', eigentuemerVorname: 'Peter', eigentuemerNachname: 'Jura'})
+  new Resource('/api/patients/Strolch',
+    {name: 'Strolch',
+      tierart: 'Hund',
+      rasse: 'Mischling',
+      diagnose: 'Bisswunde',
+      eigentuemerVorname: 'Hans',
+      eigentuemerNachname: 'Iten'}),
+  new Resource('/api/patients/Minka',
+    {name: 'Minka',
+      tierart: 'Katze',
+      rasse: 'Siam',
+      diagnose: 'Vergiftung',
+      eigentuemerVorname: 'Maria',
+      eigentuemerNachname: 'Rogenmoser'}),
+  new Resource('/api/patients/Blacky',
+    {name: 'Blacky',
+      tierart: 'Pferd',
+      rasse: 'Freiberger',
+      diagnose: 'Stauchung',
+      eigentuemerVorname: 'Peter',
+      eigentuemerNachname: 'Jura'})
 );
 
 // Mockdaten für api/journals laden
 
 server.dataStore.save(
-  new Resource('/api/journals/Strolch', {name: 'Journal Strolch', 'patient.id': 'Hund', diagnose: 'Bisswunde', startdatum: '01.11.2016', enddatum: '10.11.2016'}),
-  new Resource('/api/journals/Minka', {name: 'Journal Minka', 'patient.id': 'Katze', diagnose: 'Bisswunde', startdatum: '01.11.2016', enddatum: '10.11.2016'}),
-  new Resource('/api/journals/Blacky', {name: 'Journal Blacky', tierart: 'Pferd', rasse: 'Freiberger', diagnose: 'Stauchung', eigentuemerVorname: 'Peter', eigentuemerNachname: 'Jura'})
+  new Resource('/api/journals/Strolch',
+    {name: 'Journal Strolch', 'patient.id': 'Hund',
+      diagnose: 'Bisswunde', startdatum: '01.11.2016', enddatum: '10.11.2016'}),
+  new Resource('/api/journals/Minka',
+    {name: 'Journal Minka', 'patient.id': 'Katze',
+      diagnose: 'Bisswunde', startdatum: '01.11.2016', enddatum: '10.11.2016'}),
+  new Resource('/api/journals/Blacky',
+    {name: 'Journal Blacky', tierart: 'Pferd',
+      rasse: 'Freiberger', diagnose: 'Stauchung', eigentuemerVorname: 'Peter', eigentuemerNachname: 'Jura'})
 );
 
 server.listen(port, () => {
