f0a709f7836bbeb0585b89d97ddbcd2ee7865442
Marc Labud <marc.labud@hsr.ch>
2016-11-08 17:03:40 +0100

    changed to popup datepicker setup bhj-datepicker.component with ng2-bootstrap datepicker

--- src/app/shared/component/bhj-datepicker/bhj-datepicker.component.ts
+++ src/app/shared/component/bhj-datepicker/bhj-datepicker.component.ts
@@ -1,80 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import * as moment from 'moment';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-bhj-datepicker',
   templateUrl: './bhj-datepicker.component.html',
   styleUrls: ['./bhj-datepicker.component.css']
 })
-export class BhjDatepickerComponent implements OnInit {
-  public dt: Date = new Date();
-  public minDate: Date = void 0;
-  public events: Array<any>;
-  public tomorrow: Date;
-  public afterTomorrow: Date;
-  public formats: Array<string> = ['DD-MM-YYYY', 'YYYY/MM/DD', 'DD.MM.YYYY', 'shortDate'];
-  public format: string = this.formats[0];
-  public dateOptions: any = {
-    formatYear: 'YY',
-    startingDay: 1
-  };
-  private opened: boolean = false;
+export class BhjDatepickerComponent  {
+  @Input()
+  dateModel: Date;
+  @Input()
+  label: string;
+  @Output()
+  dateModelChange: EventEmitter<string> = new EventEmitter();
+  private showDatepicker: boolean = false;
 
-  public constructor() {
-    (this.tomorrow = new Date()).setDate(this.tomorrow.getDate() + 1);
-    (this.afterTomorrow = new Date()).setDate(this.tomorrow.getDate() + 2);
-    (this.minDate = new Date()).setDate(this.minDate.getDate() - 1000);
-    this.events = [
-      {date: this.tomorrow, status: 'full'},
-      {date: this.afterTomorrow, status: 'partially'}
-    ];
+  showPopup() {
+    this.showDatepicker = true;
   }
 
-  ngOnInit() {
-  }
-  public getDate(): number {
-    return this.dt && this.dt.getTime() || new Date().getTime();
-  }
-
-  public today(): void {
-    this.dt = new Date();
-  }
-
-  public d20090824(): void {
-    this.dt = moment('2009-08-24', 'YYYY-MM-DD').toDate();
-  }
-
-  // todo: implement custom class cases
-  public getDayClass(date: any, mode: string): string {
-    if (mode === 'day') {
-      let dayToCheck = new Date(date).setHours(0, 0, 0, 0);
-
-      for (let i = 0; i < this.events.length; i++) {
-        let currentDay = new Date(this.events[i].date).setHours(0, 0, 0, 0);
-
-        if (dayToCheck === currentDay) {
-          return this.events[i].status;
-        }
-      }
-    }
-
-    return '';
-  }
-
-  public disabled(date: Date, mode: string): boolean {
-    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-  }
-
-  public open(): void {
-    this.opened = !this.opened;
-  }
-
-  public clear(): void {
-    this.dt = void 0;
-  }
-
-  public toggleMin(): void {
-    this.dt = new Date(this.minDate.valueOf());
+  hidePopup(event) {
+    this.showDatepicker = false;
+    this.dateModel = event;
+    this.dateModelChange.emit(event);
   }
 }
 
