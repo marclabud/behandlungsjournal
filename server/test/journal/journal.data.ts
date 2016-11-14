1370a23c7f26717c34912b1c50ea44a272b3a484
Marc Labud <marc.labud@hsr.ch>
2016-11-14 17:46:24 +0100

    html5-Datepicker chrome ok

--- server/test/journal/journal.data.ts
+++ server/test/journal/journal.data.ts
@@ -7,22 +7,22 @@ export class JournalData implements IData {
       name: 'Journal Strolch',
       patient_id: '1',
       diagnose: 'Bisswunde',
-      startdatum: '2016-11-07T08:00:00',
-      enddatum: '2016-11-16T18:00:00'
+      startdatum: '2016-11-07',
+      enddatum: '2016-11-16'
     },
     {
       name: 'Journal Minka',
       patient_id: '2',
       diagnose: 'Vergiftung',
-      startdatum: '2016-11-08T08:00:00',
-      enddatum: '2016-11-17T18:00:00'
+      startdatum: '2016-11-08',
+      enddatum: '2016-11-17'
     },
     {
       name: 'Journal Blacky',
       patient_id: '3',
       diagnose: 'Stauchung',
-      startdatum: '2016-11-09T08:00:00',
-      enddatum: '2016-11-18T18:00:00'
+      startdatum: '2016-11-09',
+      enddatum: '2016-11-18'
     }
   ];
 
