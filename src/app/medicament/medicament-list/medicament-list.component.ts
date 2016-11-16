4644afd114ec4e7dc6cd069589d0eba56f7cd067
Marc Labud <marc.labud@hsr.ch>
2016-11-16 14:35:10 +0100

    setup messageservice<Bhjournal> and subscribe in medicament-list component

--- src/app/medicament/medicament-list/medicament-list.component.ts
+++ src/app/medicament/medicament-list/medicament-list.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {Http} from '@angular/http';
+import {BhJournalService} from '../../bhjournal/service/bhjournal.service';
+import {MessageService} from '../../shared/service/message/message.service';
+import {BhJournal} from '../../bhjournal/model/bhjournal';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-medicament-list',
   templateUrl: './medicament-list.component.html',
   styleUrls: ['./medicament-list.component.css']
 })
-export class MedicamentListComponent implements OnInit {
+export class MedicamentListComponent implements OnInit, OnDestroy {
+  private messageService: MessageService<BhJournal>;
+  private subscription: Subscription;
+  private behandlungsjournal: BhJournal;
 
-  constructor() { }
 
+  constructor(http: Http, private bhjournalService: BhJournalService) {
+    this.messageService = bhjournalService.messageService;
+    this.subscription = this.messageService.Itemselected$.subscribe(
+      behandlungsjournal => {
+        this.behandlungsjournal = behandlungsjournal;
+      });
+  }
   ngOnInit() {
+  }
+  ngOnDestroy() {
+    // prevent memory leak when component destroyed
+    this.subscription.unsubscribe();
   }
 
 }
