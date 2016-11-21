96b0d5fa812b736f2755f83aa5f82621494c4b20
Marc Labud <marc.labud@hsr.ch>
2016-11-21 15:07:02 +0100

    app-datepicker component functional for HTML5-Datepicker

--- src/app/shared/component/bhj-datepicker/bhj-datepicker.component.ts
+++ src/app/shared/component/bhj-datepicker/bhj-datepicker.component.ts
@@ -15,13 +15,20 @@ export class BhjDatepickerComponent implements OnInit {
   dateModelChange: EventEmitter<string> = new EventEmitter<string>();
   private showDatepicker: boolean = false;
   private HTML5_inputtype_date_Supported = false;
-  private HTML5Date: String ;
+  private HTML5Date: String;
 
-  ngOnInit () {
+  ngOnInit() {
     // Browser auf Unterstützung vom Inputtype date prüfen
     this.HTML5_inputtype_date_Supported = Modernizr.inputtypes.date;
     if (this.HTML5_inputtype_date_Supported) {
-      this.HTML5Date = this.defaultDate.format('YYYY MM DD');
+      if (typeof this.defaultDate !== 'undefined') {
+        this.HTML5Date = this.defaultDate.format('YYYY-MM-DD');
+      } else { // Fehlerbehandlung: nimm das aktuelle Datum
+        this.HTML5Date = moment().format('YYYY-MM-DD');
+        // Todo: Fehlerbehandlung für @input defaultDate == 'undefined'
+      }
+    } else {
+      // HTML5 type=date nicht supported
     }
   }
 
