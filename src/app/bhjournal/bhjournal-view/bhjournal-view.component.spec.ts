1ec015f5470d4d7c39514426a63e9eeefa64e473
Marc Labud <marc.labud@hsr.ch>
2016-11-24 20:45:07 +0100

    All tests running

--- src/app/bhjournal/bhjournal-view/bhjournal-view.component.spec.ts
+++ src/app/bhjournal/bhjournal-view/bhjournal-view.component.spec.ts
@@ -1,19 +1,15 @@
 /* tslint:disable:no-unused-variable */
-
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {BhjournalComponent} from './bhjournal-view.component';
 import {HttpModule} from '@angular/http';
 import {BhJournalService} from '../service/bhjournal.service';
 import {FormsModule} from '@angular/forms';
 import {BhjDatepickerComponent} from '../../shared/component/bhj-datepicker/bhj-datepicker.component';
-
 import {MedicamentListComponent} from '../../medicament/medicament-list/medicament-list.component';
 import {IndikatorListComponent} from '../../indikator/indikator-list/indikator-list.component';
 import {PatientService} from '../../patient/service/patient.service';
 import {DauerComponent} from '../../shared/component/dauer/dauer.component';
+import {MedicamentDetailComponent} from '../../medicament/medicament-detail/medicament-detail.component';
 
 
 describe('Component: Bhjournal', () => {
@@ -22,9 +18,14 @@ describe('Component: Bhjournal', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BhjournalComponent, MedicamentListComponent, IndikatorListComponent, BhjDatepickerComponent, DauerComponent ],
-      imports: [ HttpModule, FormsModule ],
-      providers: [ BhJournalService , PatientService]
+      declarations: [BhjournalComponent,
+        MedicamentListComponent,
+        MedicamentDetailComponent,
+        IndikatorListComponent,
+        BhjDatepickerComponent,
+        DauerComponent],
+      imports: [HttpModule, FormsModule],
+      providers: [BhJournalService, PatientService]
     })
       .compileComponents();
   }));
