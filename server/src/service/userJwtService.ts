449b303fd8c739e6ea99dd4e493d7bbb64de1023
Marc Labud <marc.labud@hsr.ch>
2016-12-05 12:33:14 +0100

    whoIsUser implemented on Server example implementation in getallusers with username

--- server/src/service/userJwtService.ts
+++ server/src/service/userJwtService.ts
@@ -2,10 +2,10 @@
 /**
  * Service for JsonWebToken (JWT). Introduction see https://jwt.io/introduction/
  */
-
-import { sign } from 'jsonwebtoken';
+import {sign, verify} from 'jsonwebtoken';
 import {User} from './model/user';
 import {JwtKeyProvider} from './keyProviderService';
+import {Request} from 'express-serve-static-core';
 
 export class JwtUserService {
 
@@ -16,5 +16,21 @@ export class JwtUserService {
     let createdToken = sign (user, secret);
     console.log ('createJWT: ' , createdToken);
     return createdToken;
+  }
+
+  public whoIsUser(request: Request): string {
+    let username = 'null'; // null
+    let auth: any = request.header('authorisation');
+    if (auth && auth.indexOf('Bearer') >= 0) {
+      let jwt = auth.substring(7);
+      jwt = jwt.replace(/\"/g, '');
+      let decodedtoken = verify(jwt, this.keyProvider.getKey());
+      if (decodedtoken) {
+        console.log('username from token:' , decodedtoken._doc.name);
+        username = decodedtoken._doc.name;
+      }
+    }
+    return username;
+
   }
 }
