904b19751adcb16406d409c339657f440ad66322
Marc Labud <marc.labud@hsr.ch>
2016-12-07 15:32:37 +0100

    behandlungsjournal-detail new refactored, patient as Input

--- src/app/bhjournal/bhjournal-detail/bhjournal-detail.component.ts
+++ src/app/bhjournal/bhjournal-detail/bhjournal-detail.component.ts
@@ -1,7 +1,13 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {BhJournalService} from '../service/bhjournal.service';
+import {PatientService} from '../../patient/service/patient.service';
+import {MessageService} from '../../shared/service/message/message.service';
+import {Subscription} from 'rxjs/Subscription';
 import {BhJournal} from '../model/bhjournal';
 import * as moment from 'moment';
+import {Patient} from '../../patient/model/patient';
+
+
 
 
 @Component({
@@ -10,6 +16,9 @@ import * as moment from 'moment';
   styleUrls: ['bhjournal-detail.component.css']
 })
 export class BhjournalDetailComponent implements OnInit {
+  private subscriptionPatient: Subscription;
+  private messageServicePatient: MessageService<Patient>;
+  private patient: Patient = null;
   /* tslint:disable-next-line:no-unused-variable */
   private labelStart = 'Beginn der Therapie';
   /* tslint:disable-next-line:no-unused-variable */
@@ -17,13 +26,35 @@ export class BhjournalDetailComponent implements OnInit {
 
   private infoMsg = {body: '', type: 'info'};
 
+  // benötigt einen gültigen @Input bhjournal
+  // kein standalone-Aufruf möglich
+  // Neues bhjournal benötigt korrekte PatientenID für den das Journal erstellt wird.
+
   @Input() bhjournal: BhJournal = new BhJournal();
 
-  constructor(private bhjournalService: BhJournalService) {}
-
-  ngOnInit() {
+  constructor(private patientService: PatientService, private bhjournalService: BhJournalService) {
+  this.messageServicePatient = this.patientService.messageService;
+  this.subscriptionPatient = this.messageServicePatient.Itemselected$.subscribe(
+  patient => {
+    this.patient = patient;
+  });
   }
+  ngOnInit() {
+    // Prüfen auf neues BhJournal
+    if (typeof (this.bhjournal._id) === 'undefined') {
+      // Falls neu Patienten ermitteln
+      if (this.patient === null) {
+        this.patient = this.patientService.readCache();
+      }
+      this.bhjournal = this.initBhJournal(this.patient);
+    }
 
+  }
+  initBhJournal(patient: Patient): BhJournal {
+    let newBhJournal = new BhJournal();
+    console.log ('Neues BhJournal', newBhJournal);
+    return newBhJournal;
+}
   saveBhJournal(bhjournal) {
     console.log('Behandlungsjournal wird gespeichert', bhjournal);
     if (typeof(bhjournal._id) === 'undefined') {
