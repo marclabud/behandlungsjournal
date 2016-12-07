02adac21a0182cf9912ffb05bea0846cfc80ccb3
MRimbeaux <michel.rimbeaux@gmx.ch>
2016-12-07 11:23:25 +0100

    Merge branch 'master' of C:\Users\UE72498\WebstormProjects\behandlungsjournal with conflicts.

--- server/src/controller/user.controller.ts
+++ server/src/controller/user.controller.ts
@@ -4,16 +4,88 @@ import {JwtKeyProvider} from '../service/keyProviderService';
 import {User} from '../service/model/user';
 import {Request} from 'express-serve-static-core';
 
+const RBAC = require('rbac-a');
+// const CustomProvider = require('CustomProvider');
+
+// const JsonProvider = RBAC.providers.JsonProvider;
+const CustomProvider = RBAC.providers.CustomProvider;
+
+const PERMISSIONS = {
+  roles: {
+    'guest': {},
+    'reader': {
+      permissions: ['read'],
+      inherited: ['guest']
+    },
+    'writer': {
+      permissions: ['create'],
+      inherited: ['reader']
+    },
+    'editor': {
+      permissions: ['update'],
+      inherited: ['reader']
+    },
+    'director': {
+      permissions: ['delete'],
+      inherited: ['reader', 'writer', 'editor'],
+      attributes: ['test']
+    },
+    'arzt': {
+      permissions: ['manage'],
+      inherited: ['director']
+    },
+    'tierpfleger': {
+      inherited: ['reader', 'writer']
+    }
+  }
+  // users: {
+  //   'joe': ['guest'],
+  //   'jim': ['missingRole']
+  // }
+};
+
+let roles = {
+  'director': {
+    'reader': {'guest': null},
+    'editor': {'reader': null},
+    'writer': {'reader': null}
+  }
+};
+
+let rbac = new RBAC({
+  // provider: new JsonProvider(RULES)
+  provider: new CustomProvider(PERMISSIONS, roles)
+});
+
+rbac.on('error', function (err) {
+  console.error('Error while checking $s/%s', err.role, err.user);
+  console.error(err.stack);
+});
+
 const UserCollection = require('../models/user.model');
 
 module.exports.getAllUsers = (request, response) => {
-  let user = whoIsUser(request);
-  console.log ('getallusers: usernamefromtoken', user);
-  UserCollection.find({}, (err, docs) => {
-    if (err) {
-      return console.error(err);
+  let username = whoIsUser(request);
+  console.log('getallusers: usernamefromtoken', username);
+
+  rbac.check('joe', 'manage').then(function (allowed) {
+      if (allowed) {
+        UserCollection.find({}, (err, docs) => {
+          if (err) {
+            return console.error(err);
+          }
+          response.status(200).json(docs);
+        });
+        console.log('User can read!');
+      } else {
+        console.log('User cannot read.');
+        console.log('Please contact your system admin for more information');
+        return response.status(401).send('User has no rights');
+      }
     }
-    response.status(200).json(docs);
+  ).catch(function (err) {
+    console.error(err && err.stack || err || 'ERROR');
+    return response.status(200);
   });
 };
 
@@ -79,16 +151,16 @@ module.exports.loginUser = (request, response) => {
         return console.error(err);
       }
 
-    if (1 === (docs.length)) {
-      // genau 1 User wurde gefunden; Token wird zurückgesendet
-      let user: User = docs[0];
-      let token = getjwtToken(user);
-      response.status(201).send({id_token: token});
-      console.log('Token', 'Token to response ok');
-    } else {    // keinen oder mehr als einen User mit diesem Passwort gefunden
-      return response.status(401).send({message: 'The username or password dont match', email: email});
-    }
-  });
+      if (1 === (docs.length)) {
+        // genau 1 User wurde gefunden; Token wird zurückgesendet
+        let user: User = docs[0];
+        let token = getjwtToken(user);
+        response.status(201).send({id_token: token});
+        console.log('Token', 'Token to response ok');
+      } else {    // keinen oder mehr als einen User mit diesem Passwort gefunden
+        return response.status(401).send({message: 'The username or password dont match', email: email});
+      }
+    });
 };
 
 function getjwtToken(user: User): string {
@@ -97,7 +169,7 @@ function getjwtToken(user: User): string {
   return jwtUserservice.createJWT(user);
 }
 
-function whoIsUser(request: Request): User {
+function whoIsUser(request: Request): string {
   let keyProvider = new JwtKeyProvider();
   let jwtUserService = new JwtUserService(keyProvider);
   return jwtUserService.whoIsUser(request);
