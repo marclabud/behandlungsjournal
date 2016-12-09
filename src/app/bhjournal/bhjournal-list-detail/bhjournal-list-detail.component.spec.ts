15ca912c7d6ce1168230445497625605e8fa4085
Marc Labud <marc.labud@hsr.ch>
2016-12-09 16:44:21 +0100

    tests refactored for sucess

--- src/app/bhjournal/bhjournal-list-detail/bhjournal-list-detail.component.spec.ts
+++ src/app/bhjournal/bhjournal-list-detail/bhjournal-list-detail.component.spec.ts
@@ -1,9 +1,19 @@
+///<reference path="../bhjournal-detail/bhjournal-detail.component.ts"/>
+///<reference path="../bhjournal-list/bhjournal-list.component.ts"/>
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { BhjournalListDetailComponent } from './bhjournal-list-detail.component';
+import {BhjournalDetailComponent} from '../bhjournal-detail/bhjournal-detail.component';
+import {BhjournalListComponent} from '../bhjournal-list/bhjournal-list.component';
+import {DauerModule} from '../../shared/component/dauer/dauer.module';
+import {FormsModule} from '@angular/forms';
+import {BhJournalService} from '../service/bhjournal.service';
+import {AUTH_PROVIDERS} from 'angular2-jwt';
+import {HttpModule} from '@angular/http';
+import {PatientService} from '../../patient/service/patient.service';
 
 describe('BhjournalListDetailComponent', () => {
   let component: BhjournalListDetailComponent;
@@ -11,15 +21,17 @@ describe('BhjournalListDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BhjournalListDetailComponent ]
+      declarations: [ BhjournalListDetailComponent, BhjournalDetailComponent, BhjournalListComponent],
+      imports: [FormsModule, DauerModule, HttpModule],
+      providers: [BhJournalService, PatientService, AUTH_PROVIDERS]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BhjournalListDetailComponent);
+    fixture = TestBed.createComponent(BhjournalListDetailComponent, );
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
--- src/app/patient/patient-card/patient-card.component.spec.ts
+++ src/app/patient/patient-card/patient-card.component.spec.ts
@@ -1,9 +1,15 @@
+///<reference path="../patient-detail/patient-detail.component.ts"/>
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { PatientCardComponent } from './patient-card.component';
+import {PatientDetailComponent} from '../patient-detail/patient-detail.component';
+import {FormsModule} from '@angular/forms';
+import {PatientService} from '../service/patient.service';
+import {HttpModule} from '@angular/http';
+import {AUTH_PROVIDERS} from 'angular2-jwt';
 
 describe('PatientCardComponent', () => {
   let component: PatientCardComponent;
@@ -11,7 +17,9 @@ describe('PatientCardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PatientCardComponent ]
+      declarations: [ PatientCardComponent, PatientDetailComponent ],
+      imports: [FormsModule, HttpModule],
+      providers: [PatientService, AUTH_PROVIDERS]
     })
     .compileComponents();
   }));
@@ -19,7 +27,7 @@ describe('PatientCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PatientCardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
