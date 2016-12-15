315ef8413f573516eb12de15f9ed99ee5d1d66c1
Marc Labud <marc.labud@hsr.ch>
2016-12-15 21:38:34 +0100

    BhJournal redirects to bhjournal creation if bhjournal is null

--- src/app/bhjournal/bhjournal-view/bhjournal-view.component.ts
+++ src/app/bhjournal/bhjournal-view/bhjournal-view.component.ts
@@ -7,6 +7,7 @@ import {PatientService} from '../../patient/service/patient.service';
 import {BhJournal} from '../model/bhjournal';
 import * as moment from 'moment';
 import {Dauer} from '../../shared/model/dauer';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-bhjournal',
@@ -30,7 +31,7 @@ export class BhjournalComponent implements OnInit, OnDestroy {
   /* tslint:disable-next-line:no-unused-variable */
   private labelTherapieEnde = 'Ende der Therapie:';
 
-  constructor(private bhjournalService: BhJournalService, private patientService: PatientService) {
+  constructor(private bhjournalService: BhJournalService, private patientService: PatientService, private router: Router) {
     this.messageServiceBhJournal = bhjournalService.messageService;
     this.messageServicePatient = patientService.messageService;
     this.subscriptionPatient = this.messageServicePatient.Itemselected$.subscribe(
@@ -72,16 +73,17 @@ export class BhjournalComponent implements OnInit, OnDestroy {
   private getJournals(journals: Array<BhJournal>) {
     console.log('getJournals Parameter journals: ', journals);
     this.journalsUTC = journals;
-    if (0 !== journals.length) {
+    if (journals.length) {
       this.selectedBhJournal = this.journalsUTC[0];
       this.patient_id = this.selectedBhJournal._id;
       // TODO: ein Patient kann mehrere Journale haben, wenn sortiert nach Jüngstem wäre 1. element ok sonst nok !!
       this.therapiedauer.startDatum = moment.utc(this.selectedBhJournal.dauer.startDatum);
       this.therapiedauer.endeDatum = moment.utc(this.selectedBhJournal.dauer.endeDatum);
       this.messageServiceBhJournal.selectItem(this.selectedBhJournal);
+    } else { // Kein BhJournal vorhanden, bhjournal anlegen
+       this.router.navigate(['/bhjournal-list-detail']);
     }
   };
-
   ngOnDestroy() {
     // prevent memory leak when component destroyed
     this.subscriptionPatient.unsubscribe();
