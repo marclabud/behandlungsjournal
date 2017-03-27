e5da7cf1919a93e29e14c0bb259430af7b5f6699
Marc Labud <marc.labud@hsr.ch>
2017-03-27 08:51:50 +0200

    Patient Search moved to Patient menu

--- src/app/patient/patient-menu/patient-menu.component.ts
+++ src/app/patient/patient-menu/patient-menu.component.ts
@@ -4,6 +4,8 @@ import {MessageService} from '../../shared/service/message/message.service';
 import {Patient} from '../model/patient';
 import {Subscription} from 'rxjs';
 import {Router} from '@angular/router';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {SearchService} from '../../shared/component/service/search.service';
 
 @Component({
   selector: 'app-patient-menu',
@@ -11,16 +13,24 @@ import {Router} from '@angular/router';
   styleUrls: ['./patient-menu.component.scss']
 })
 export class PatientMenuComponent implements OnInit {
+
+  searchForm: FormGroup;
+  searchTerm: string;
+  private PatientsearchIsVisible=false;
   private messageServicePatient: MessageService<Patient>;
   private subscriptionPatient: Subscription;
   private patient: Patient;
 
   @Output() private patientSelectionWithSearch: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  constructor(private patientService: PatientService, private router: Router) {
+  constructor(private search: SearchService,private patientService: PatientService, private formBuilder: FormBuilder, private router: Router) {
+    this.searchForm = formBuilder.group({
+      searchTermCtrl: '',
+    });
     this.messageServicePatient = patientService.messageService;
     this.subscriptionPatient = this.messageServicePatient.Itemselected$.subscribe(
       patient => {
+        this.PatientsearchIsVisible=false;
         this.patient = patient;
         this.patientSelectionWithSearch.emit(false);
         this.router.navigate(['/bhjournal']);
@@ -28,10 +38,17 @@ export class PatientMenuComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.searchForm.valueChanges.subscribe(value => {
+      this.getSearchTerm(value);
+    });
+    this.searchForm.statusChanges.subscribe(value => {
+      console.log('searchTerm Value', value);
+    });
     this.getSelectedPatient();
   }
 
   onChoosePatient() {
+    this.PatientsearchIsVisible=true;
     this.patientSelectionWithSearch.emit(true);
     this.patient.name = 'Kein Patient';
     this.router.navigate(['/patient-card']);
@@ -43,10 +60,14 @@ export class PatientMenuComponent implements OnInit {
       this.router.navigate(['/bhjournal']);
     } else {
       this.patient = new Patient();
-      this.patient.name = 'Kein Patient';
-      this.router.navigate(['/patient-card']);
-
+      this.onChoosePatient();
     }
   }
-
+  getSearchTerm(search) {
+    console.log('searchTerm Value', search);
+    if (search) {
+      this.searchTerm = search.searchTermCtrl;
+      this.search.setSearchTerm(this.searchTerm);
+    }
+  }
 }
