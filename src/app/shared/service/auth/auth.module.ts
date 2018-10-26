eca3a7cf9e36b8884f437bbfd03be92839478908
Marc Labud <marc.labud@hsr.ch>
2018-10-26 21:51:23 +0200

    build --prod tokengetter function

--- src/app/shared/service/auth/auth.module.ts
+++ src/app/shared/service/auth/auth.module.ts
@@ -1,11 +1,15 @@
 import {NgModule} from '@angular/core';
 import {JwtModule} from '@auth0/angular-jwt';
 
+export function jwtTokenGetter() {
+    return sessionStorage.getItem('token');
+}
+
 @NgModule({
     imports: [
         JwtModule.forRoot({
             config: {
-                tokenGetter: (() => sessionStorage.getItem('token')),
+                tokenGetter: jwtTokenGetter,
                 whitelistedDomains: ['localhost:3000'],
             }
         }),
